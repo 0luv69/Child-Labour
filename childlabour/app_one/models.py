214821829b08a1a94aef9c43fe112d219ad16722
0luv69 <baniyarujal@gmail.com>
2024-06-08 10:24:00 +0545

    migrated

--- childlabour/app_one/models.py
+++ childlabour/app_one/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import uuid
+from ..childlabour import settings
+from django.core.mail import send_mail
 
 # Create your models here.
+
+
+class Complaint (models.Model):
+    title = models.CharField(max_length=250)
+    image = models.CharField( max_length=100)
+    location = models.CharField( max_length=100)
+    email = models.EmailField( max_length=250)
+    Type_list = (
+        ('sexual-abuse', 'Sexual Abuse'),
+        ('forced-labor', 'Forced Labor'),
+        ('domestic-servitude', 'Domestic Servitude'),
+        ('agricultural-labor', 'Agricultural Labor'),
+        ('industrial-work', 'Industrial Work'),
+        ('mining', 'Mining'),
+        ('street-work', 'Street Work'),
+        ('bonded-labor', 'Bonded Labor'),
+        ('child-soldiering', 'Child Soldiering'),
+        ('trafficking', 'Trafficking'),
+        ('informal-sector-work', 'Informal Sector Work'),
+        ('construction-work', 'Construction Work'),
+    )
+    
+    type = models.CharField( max_length=100, choices=Type_list, default= 'forced-labor')
+
+    email_token = models.CharField(max_length=100, null= True, blank= True)
+    email_verified = models.BooleanField(default=False)
+    admin_verified = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+
+@receiver(post_save, sender= Complaint)
+def send_email_token(sender, instance, created, **kwargs):
+    try:
+        if created:
+            email_token = str(uuid.uuid4())
+            email = instance.email 
+            Complaint.objects.filter(id=instance.id).update(email_token=email_token)
+            send_account_activation_email(email, email_token, instance)
+    except Exception as E:
+        print(E)
+
+def send_account_activation_email(email, email_token, complaint_instance):
+    subject= 'lets verify so click the link plz'
+    email_from = settings.EMAIL_HOST_USER
+    message = f'''Hi, {complaint_instance.email} Your authentation url is plz open to be verified user, thank
+    click on the link to Activate Your account {email_token}
+    thanks
+'''
+
+    send_mail(subject, message, email_from, [email])
--- childlabour/app_one/views.py
+++ childlabour/app_one/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
+from .models import *
 
 # Create your views here.
+
+
+def activate_email(request,emailtoken):
+    try:
+        user_profile= Complaint.objects.get(email_token=emailtoken) 
+        user_profile.is_email_verified= True
+        user_profile.save()
+    except:
+        return HttpResponse('Sorry but wrong authentation key,.....................................................')
+
+    return redirect('/')
